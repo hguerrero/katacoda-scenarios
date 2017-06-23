5643dd130443f075902cbbcc1b6020b97d0fc120
hguerrero <hguerreroo@yahoo.com>
2017-06-23 14:09:36 -0500

    again

--- amq/getting-started-broker/env-init.sh
+++ amq/getting-started-broker/env-init.sh
@@ -1,4 +1,3 @@
-ssh root@host01 "mkdir /opt/amq && chown scrapbook:scrapbook /opt/amq"
 wget https://raw.githubusercontent.com/hguerrero/katacoda-scenarios/master/amq/getting-started-broker/assets/jboss-amq-7.0.0.redhat-1-bin.zip
-unzip jboss-amq-7.0.0.redhat-1-bin.zip -d /opt/amq/
+unzip jboss-amq-7.0.0.redhat-1-bin.zip
 rm jboss-amq-7.0.0.redhat-1-bin.zip
