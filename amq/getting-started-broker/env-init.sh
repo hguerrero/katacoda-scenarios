cef76f75689612e7f466a21fecdf93fabbc28145
hguerrero <hguerreroo@yahoo.com>
2017-06-26 11:52:55 +0100

    create-broker

--- amq/getting-started-broker/env-init.sh
+++ amq/getting-started-broker/env-init.sh
@@ -1,3 +1,6 @@
 wget https://raw.githubusercontent.com/hguerrero/katacoda-scenarios/master/amq/getting-started-broker/assets/jboss-amq-7.0.0.redhat-1-bin.zip
 unzip jboss-amq-7.0.0.redhat-1-bin.zip
 rm jboss-amq-7.0.0.redhat-1-bin.zip
+cd jboss-amq-7.0.0.redhat-1
+bin/artemis create --host 0.0.0.0 --allow-anonymous --user admin --password password --http-host 0.0.0.0 instances/simplebroker
+cd instances/simplebroker
