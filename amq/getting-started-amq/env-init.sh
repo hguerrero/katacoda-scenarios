0f155900c17fa363be70b41bc23ed201d06e031c
Hugo Guerrero <hguerrer@ovpn-124-216.rdu2.redhat.com>
2017-11-15 16:07:31 -0500

    changing to tag for current OCP version (3.6)

--- amq/getting-started-amq/env-init.sh
+++ amq/getting-started-amq/env-init.sh
@@ -1,7 +1,8 @@
 ssh root@host01 "oc project openshift"
-ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-basic.json -n openshift"
+ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/amq/amq63-basic.json -n openshift"
 ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift"
 ssh root@host01 "docker pull registry.access.redhat.com/jboss-amq-6/amq63-openshift:latest"
 ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
 ssh root@host01 "oc new-project messaging-queues"
-ssh root@host01 "oc new-app amq63-basic -P MQ_PROTOCOL=openwire,amqp,stomp -P MQ_QUEUES=myqueue -P MQ_TOPICS=mytopic -P MQ_USERNAME=amqUser -P MQ_PASSWORD=amqPassword"
+ssh root@host01 "oc policy add-role-to-user admin developer -n messaging-queues"
+ssh root@host01 "oc new-app amq63-basic -p MQ_PROTOCOL=openwire,amqp,stomp -p MQ_QUEUES=myqueue -p MQ_TOPICS=mytopic -p MQ_USERNAME=amqUser -p MQ_PASSWORD=amqPassword"
