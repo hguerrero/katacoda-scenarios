7a3b9a14d3f97cdee6a295c1717d840e42d2d41d
Hugo Guerrero <hguerrer@ovpn-124-216.rdu2.redhat.com>
2017-11-15 15:31:43 -0500

    create instead of update

--- amq/getting-started-amq/env-init.sh
+++ amq/getting-started-amq/env-init.sh
@@ -1,5 +1,4 @@
-ssh root@host01 "oc project openshift"
-ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-basic.json"
-ssh root@host01 "oc replace -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json"
+ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/amq/amq63-basic.json -n openshift"
+ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift"
 ssh root@host01 "docker pull registry.access.redhat.com/jboss-amq-6/amq63-openshift:latest"
 ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
