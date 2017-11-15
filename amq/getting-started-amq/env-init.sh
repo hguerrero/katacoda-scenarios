13b4cc228cc96cf48092131d70c2ad0c69f54635
Hugo Guerrero <hguerrer@ovpn-124-216.rdu2.redhat.com>
2017-11-15 15:55:45 -0500

    added new-project new-app for messaging queues

--- amq/getting-started-amq/env-init.sh
+++ amq/getting-started-amq/env-init.sh
@@ -3,3 +3,5 @@ ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/
 ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/jboss-image-streams.json -n openshift"
 ssh root@host01 "docker pull registry.access.redhat.com/jboss-amq-6/amq63-openshift:latest"
 ssh root@host01 'for i in {1..200}; do oc policy add-role-to-user system:image-puller system:anonymous && break || sleep 1; done'
+ssh root@host01 "oc new-project messaging-queues"
+ssh root@host01 "oc new-app amq63-basic -P MQ_PROTOCOL=openwire,amqp,stomp -P MQ_QUEUES=myqueue -P MQ_TOPICS=mytopic -P MQ_USERNAME=amqUser -P MQ_PASSWORD=amqPassword"
