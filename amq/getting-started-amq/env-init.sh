98bf6df35692767d88733ce772f724de44f2c0db
Hugo Guerrero <hguerrer@redhat.com>
2018-04-10 12:36:04 -0400

    added just amq image stream

--- amq/getting-started-amq/env-init.sh
+++ amq/getting-started-amq/env-init.sh
@@ -1,2 +1,3 @@
 ssh root@host01 "oc project openshift"
 ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/amq/amq63-persistent-ssl.json -n openshift"
+ssh root@host01 "oc create -f /opt/amq63-image-stream.json -n openshift"
