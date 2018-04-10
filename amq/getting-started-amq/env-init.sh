33a5a74f73ba1d224fc4740fbfeeb014639eaea7
Hugo Guerrero <hguerrer@redhat.com>
2018-04-10 11:19:07 -0400

    back to previous version to reuse the current env.

--- amq/getting-started-amq/env-init.sh
+++ amq/getting-started-amq/env-init.sh
@@ -1,3 +1,2 @@
 ssh root@host01 "oc project openshift"
-ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.7/amq/amq63-persistent-ssl.json -n openshift"
-ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.7/jboss-image-streams.json -n openshift"
+ssh root@host01 "oc create -f https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.5/amq/amq63-persistent-ssl.json -n openshift"
