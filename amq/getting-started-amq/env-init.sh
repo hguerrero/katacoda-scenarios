3bc6fb4f9261610a5f8be75b4f3534fef850b872
Hugo Guerrero <hguerrer@redhat.com>
2018-05-03 20:19:35 -0500

    added required components

--- amq/getting-started-amq/env-init.sh
+++ amq/getting-started-amq/env-init.sh
@@ -1,4 +1,6 @@
-ssh root@host01 "mkdir -p /root/projects && cd /root/projects && git clone https://github.com/hguerrero/amq-examples"
+ssh root@host01 "cd /root/projects && git clone https://github.com/hguerrero/amq-examples"
+ssh root@host01 "curl -k --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -"
+ssh root@host01 "yum -y install nodejs npm"
 ssh root@host01 "oc project openshift"
 ssh root@host01 "oc create -f /opt/amq-broker-71-basic.yaml -n openshift"
 ssh root@host01 "oc create -f /opt/amq-7-image-streams.json -n openshift"
